80ade0fe08fa71c48ddf21d650aa2fa26fd5df0b
benjamin <benjamin.morales0401@gmail.com>
2025-10-14 21:57:08 -0300

    Se agrega campos para rrelanar con correo y contrasela, tambien se agrega boton para volver atras,

---
+++ app/src/main/java/com/example/appasistencia/ui/screen/IniciarSesionScreen.kt
@@ -0,0 +1,130 @@
+package com.example.appasistencia.ui.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+
+@Composable
+fun IniciarSesionScreen(
+    onLogin: () -> Unit,    // Cuando el login es exitoso
+    onBack: () -> Unit     // Para volver atrás
+) {
+    var correo by remember { mutableStateOf("") }
+    var contraseña by remember { mutableStateOf("") }
+
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier.fillMaxSize().
+            padding(innerPadding).
+            padding(top = 32.dp), //separar de arriba
+            horizontalAlignment = Alignment.CenterHorizontally,//centrar al medio de columna
+            verticalArrangement = Arrangement.Top //Posisiona el texto arriba
+        ) {
+            Text(
+                text = "AsisTrack",
+                fontSize = 40.sp,// tmaño Letra
+                modifier = Modifier.padding(top = 50.dp)// separa el segundo texto del Primero
+            )
+
+
+
+            Text(
+                text = "Iniciar Sesion ",
+                fontSize = 16.sp,// tmaño Letra
+                modifier = Modifier.padding(top = 5.dp)// separa el segundo texto del Primero
+            )
+
+            Text(
+                text = "Correo ",
+                fontSize = 16.sp,// tmaño Letra
+                modifier = Modifier.padding(top = 50.dp)// separa el segundo texto del Primero
+                    .align(Alignment.Start) // Alineado a la izquierda
+                    .fillMaxWidth()
+                    .padding(start = 16.dp)// lo separa de la orilla
+                    .padding(bottom = 8.dp)
+            )
+
+            OutlinedTextField(
+
+                value = correo,
+                onValueChange = { newValue -> correo = newValue },
+                label = { Text("Correo") },
+                placeholder = { Text("Escriba aca su Correo") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .padding(bottom = 16.dp),
+                singleLine = true
+
+            )
+
+            Text(
+                text = "Contraseña ",
+                fontSize = 16.sp,// tmaño Letra
+                modifier = Modifier.padding(top = 30.dp)// separa el segundo texto del Primero
+                    .align(Alignment.Start) // Alineado a la izquierda
+                    .fillMaxWidth()
+                    .padding(start = 16.dp)// lo separa de la orilla
+                    .padding(bottom = 8.dp)
+            )
+
+            OutlinedTextField(
+                value = contraseña,
+                onValueChange = { newValue -> contraseña = newValue },
+                label = { Text("Contraseña") },
+                placeholder = { Text("Escriba aca su Contraseña") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp) //separa de la orilla
+                    .padding(bottom = 16.dp),
+                singleLine = true
+            )
+
+
+            Button(
+                onClick = onLogin,
+                modifier = Modifier
+                    .padding(top = 100.dp)// separacion del boton iniciiar secion
+                    .width(250.dp) // ancho boton
+                    .height(60.dp) // alto boton
+
+            ) {
+                Text("Iniciar Sesion")
+            }
+
+            OutlinedButton(
+                onClick = onBack,
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .width(250.dp)
+                    .height(60.dp)
+            ) {
+                Text("Volver al Inicio")
+            }
+
+        }
+    }
+}
+
+
+
+
