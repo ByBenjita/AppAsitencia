bdd309d1d8810ab5fcaf7090e6f03abb8687229f
benjamin <benjamin.morales0401@gmail.com>
2025-10-15 17:30:59 -0300

    Se agregan referencias tanto a loginviewModel, y se crea State para guardar datos que pueden variar.

--- app/src/main/java/com/example/appasistencia/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/example/appasistencia/ui/screen/LoginScreen.kt
@@ -13,23 +13,27 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.appasistencia.viewmodel.LoginViewModel
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
 
 @Composable
 fun LoginScreen(
     onLogin: () -> Unit,    // Cuando el login es exitoso
     onBack: () -> Unit     // Para volver atrás
 ) {
-    var correo by remember { mutableStateOf("") }
-    var contraseña by remember { mutableStateOf("") }
+    val viewModel: LoginViewModel = viewModel ()
+    val state by viewModel.state.collectAsState()
+
+
 
     Scaffold { innerPadding ->
         Column(
@@ -65,17 +69,31 @@ fun LoginScreen(
 
             OutlinedTextField(
 
-                value = correo,
-                onValueChange = { newValue -> correo = newValue },
+                value = state.correo,
+                onValueChange = { newValue ->
+                    viewModel.onEmailChange(newValue)
+                },
                 label = { Text("Correo") },
-                placeholder = { Text("Escriba aca su Correo") },
+                placeholder = { Text("Escriba aca su Correo")},
+                isError = state.correoError != null,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
                     .padding(bottom = 16.dp),
                 singleLine = true
-
             )
+            //Mensaje de error de correo
+            state.correoError?.let { error ->
+                Text(
+                    text = error,
+                    color = androidx.compose.material3.MaterialTheme.colorScheme.error,
+                    fontSize = 14.sp,
+                    modifier = Modifier
+                        .align(Alignment.Start)
+                        .fillMaxWidth()
+                        .padding(start = 16.dp, bottom = 8.dp)
+                )
+            }
 
             Text(
                 text = "Contraseña ",
@@ -88,16 +106,49 @@ fun LoginScreen(
             )
 
             OutlinedTextField(
-                value = contraseña,
-                onValueChange = { newValue -> contraseña = newValue },
+                value = state.contraseña,
+                onValueChange = { newValue ->
+                    viewModel.onPasswordChange(newValue)
+                },
                 label = { Text("Contraseña") },
                 placeholder = { Text("Escriba aca su Contraseña") },
+                isError = state.contraseñaError !=null,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp) //separa de la orilla
                     .padding(bottom = 16.dp),
                 singleLine = true
             )
+                //Mensaje de error de contraseña
+            state.contraseñaError?.let { error ->
+                Text(
+                    text = error,
+                    color = androidx.compose.material3.MaterialTheme.colorScheme.error,
+                    fontSize = 14.sp,
+                    modifier = Modifier
+                        .align(Alignment.Start)
+                        .fillMaxWidth()
+                        .padding(start = 16.dp, bottom = 8.dp)
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .padding(top = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = state.rememberMe,
+                    onCheckedChange = viewModel::onRememberMeChange
+                )
+                Text(
+                    text = "Gurdar inicio de sesión",
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+            }
 
 
             Button(
