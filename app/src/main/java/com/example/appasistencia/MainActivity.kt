1e4f1de578fd0f00d7e4332de5af9b707f7f11b9
benjamin <benjamin.morales0401@gmail.com>
2025-10-15 16:03:21 -0300

    Se modifica main, y se agregan la referencias para navegar entre pantallas

--- app/src/main/java/com/example/appasistencia/MainActivity.kt
+++ app/src/main/java/com/example/appasistencia/MainActivity.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.appasistencia.ui.screen.InicioApp
+import androidx.navigation.compose.rememberNavController
 import com.example.appasistencia.ui.theme.AppAsistenciaTheme
+import com.example.appasistencia.navigation.NavGraph
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,25 +15,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppAsistenciaTheme {
-                InicioApp(
-                    onGoLogin = {
-
-                    }
-                )
+                    val navController = rememberNavController()
+                    NavGraph(navController = navController)
             }
         }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun InicioAppPreview() {
-    AppAsistenciaTheme {
-        InicioApp(
-            onGoLogin = {
-            }
-        )
-    }
-}
+
+
 
 
