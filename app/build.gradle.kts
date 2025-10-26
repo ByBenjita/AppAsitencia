635423ba2e1899db787bc795d7c6e315169221cf
benjmain <benjamin.morales0401@gmail.com>
2025-10-26 15:47:16 -0300

    Se implementan las rutas para implementar mapa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,12 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
     implementation(libs.androidx.navigation.compose)
+
+    //Para la implementacion del mapa
+    implementation("org.osmdroid:osmdroid-android:6.1.14")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
