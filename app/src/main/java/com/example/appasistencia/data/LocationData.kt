38dcc2ff8b400046d78a64906fe8a8be2a856b54
benjmain <benjamin.morales0401@gmail.com>
2025-10-26 15:49:46 -0300

    Se implementa LocationData.kt para agregar ubicacion predefinidas en la app

---
+++ app/src/main/java/com/example/appasistencia/data/LocationData.kt
@@ -0,0 +1,17 @@
+package com.example.appasistencia.data
+
+
+data class AllowedLocation(
+    val name: String,
+    val latitude: Double,
+    val longitude: Double
+)
+
+// Ejemplo de datos de prueba (puedes poner tu dirección o la de tu empresa)
+object PerimtidasLocation {
+    val sampleLocations = listOf(
+        AllowedLocation("Sta. Elena de Huechuraba 1660,Región Metropolitana", -33.36340413045603,  -70.67818520352537), // Duoc Plaza Norte
+        AllowedLocation("Casa", -33.350261,  -70.880225) // Mi casa
+    )
+}
+
