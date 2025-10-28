7dba0c7dcc55686ef6a1d3bfa0cbb69bb5549412
benjmain <benjamin.morales0401@gmail.com>
2025-10-27 23:22:35 -0300

    Se agrega referencoa necesaria para implemetnacion de mapa

--- app/src/main/java/com/example/appasistencia/MainActivity.kt
+++ app/src/main/java/com/example/appasistencia/MainActivity.kt
@@ -7,11 +7,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
 import com.example.appasistencia.ui.theme.AppAsistenciaTheme
 import com.example.appasistencia.navigation.NavGraph
-
+import org.osmdroid.config.Configuration
+import androidx.preference.PreferenceManager
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        Configuration.getInstance().load(
+            applicationContext,
+            PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        )
+        Configuration.getInstance().userAgentValue = packageName
+
+
         enableEdgeToEdge()
         setContent {
             AppAsistenciaTheme {
