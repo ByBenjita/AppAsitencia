7e254db54fd79027ec0c5f1761199806d8c197b7
Patricio <patriciojimenez838@gmail.com>
2025-11-21 16:12:03 -0300

    Refactor: Se integra API para mostrar datos del perfil
    Se añaden las dependencias de Retrofit y se refactoriza la pantalla de perfil para consumir los datos de un ViewModel, reemplazando la información estática. Se gestionan los estados de carga, error y éxito en la obtención de los datos del usuario.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,14 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.compose.material3:material3:1.3.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
 }
